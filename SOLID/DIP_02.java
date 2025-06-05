530b06dcb44c20d91b2f396650a16bf96617be06
anusha-dev13 <anushadpoojari@gmail.com>
2025-06-06 00:28:02 +0200

    TASK 2

--- SOLID/DIP_02.java
+++ SOLID/DIP_02.java
@@ -3,37 +3,33 @@ package SOLID;
 public class DIP_02 {
 
     /*
-     * TASK:
-     * The class Employee violates the DIP (Dependency Inversion Principle).
-     * Fix this!
+     * FIXED: The Employee class now depends on the Notification interface,
+     * not on the EmailNotification class. This follows DIP.
      */
-    
+
     public static interface Notification {
         void doNotify();
     }
-
     public static class EmailNotification implements Notification {
         @Override
         public void doNotify() {
             System.out.println("Sending notification via email!");
         }
     }
-
     public static class Employee {
-        private EmailNotification emailNotification;
+        private Notification notification; 
 
-        // Dependency Injection (again) composition
-        public Employee(EmailNotification emailNotification) {
-            this.emailNotification = emailNotification;
+        public Employee(Notification notification) {
+            this.notification = notification;
         }
-    
+
         public void notifyEmployee() {
-            emailNotification.doNotify();
+            notification.doNotify();
         }
     }
 
     public static void main(String[] args) {
-        EmailNotification emailNotification = new EmailNotification();
+        Notification emailNotification = new EmailNotification(); 
         Employee employee = new Employee(emailNotification);
         employee.notifyEmployee();
     }
